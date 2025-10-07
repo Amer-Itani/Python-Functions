271789e8f51ee957303235c9f5292a84b9805986
Amer Itani <itaniamer4@gmail.com>
2025-10-07 16:53:05 +0300

    Marked time complexity with comments

--- algorithms.py
+++ algorithms.py
@@ -1,34 +1,47 @@
+# O(1) + O(n) * O(1) => O(n)
 def factorial(n):
+    #O(1)
     result = 1
+
     if n == 0:
         return result
     elif n > 0:
+        #O(n) n being the number of iterations from 1 to n.
         for i in range(n, 1, -1):
+            #O(1)
             result *= i
         return result
     return
-
+# O(1) + O(n) * O(1) => O(n)
 def find_max(numbers):
+    #O(1)
     maximum = numbers[0]
 
+    #O(n) n being the length of the list numbers.
     for i in range(1, len(numbers)):
+        #O(1)
         if numbers[i] > maximum:
             maximum = numbers[i]
     return maximum
-
+#O(n) * O(1) + O(1) => O(n)
 def linear_search(numbers, target):
+    #O(n) n being the length of the list numbers.
     if target in numbers:
+        #O(1)
         return True
     return False
-
+#O(1) + O(2 ^ n) => O(2 ^ n)
 def recursive_fibonacci(n):
+    #O(1)
     if n == 0 or n == 1:
         return n
     elif n > 1:
+        #O(2 ^ n) n being the position of the fibonacci sequence number.
         return fibonacci(n - 2) + fibonacci(n - 1)
     return
-
+#O(1) + O(n) * O(1) => O(n)
 def iterative_fibonacci(n):
+    #O(1)
     first_number = 0
     second_number = 1
     result = 0
@@ -36,16 +49,18 @@ def iterative_fibonacci(n):
     if n == 0 or n == 1:
         return n
     elif n > 1:
+        #O(n) n being the position of the fibonacci sequence number.
         for i in range(n-1):
+            #O(1)
             third_number = first_number + second_number
             first_number = second_number
             second_number = third_number
         result = third_number
         return result
     return
-
+#O(1)
 logged_in = False
-
+#O(1)
 while True:
     choice = int(input("Enter your choice number: "))
 
